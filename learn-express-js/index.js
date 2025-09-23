c5f37e0e9d95094580e5f1c7f9a4cf464405a4ad
aayush598 <aayushgid598@gmail.com>
2025-09-23 18:39:39 +0530

    learn-express: dotenv load env variable

--- learn-express-js/index.js
+++ learn-express-js/index.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world')
 })
 
 app.listen(port, () => {
-    console.log(`Example app running on port : ${port}`)
+    console.log(`Server running on port : ${port}`)
 })
